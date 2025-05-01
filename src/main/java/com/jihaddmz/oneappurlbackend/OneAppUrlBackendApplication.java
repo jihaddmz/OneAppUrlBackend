54bbf5fe1478b165803ddbee4a62c85b00b05ca7
jihaddmz <jihadmahfouz0@gmail.com>
2025-05-01 22:17:05 +0300

    - Initial Commit

---
+++ src/main/java/com/jihaddmz/oneappurlbackend/OneAppUrlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jihaddmz.oneappurlbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneAppUrlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneAppUrlBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihaddmz/oneappurlbackend/OneAppUrlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihaddmz.oneappurlbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneAppUrlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
